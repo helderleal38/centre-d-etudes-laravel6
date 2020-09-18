6b9bfd1a43d02db8aea0e209f8091b9b48950c37
helderleal <helder.leal.francisco@gmail.com>
2020-09-18 13:52:56 +0200

    news views for Student space

---
+++ resources/views/administration/students/actions/newStudent.blade.php
@@ -0,0 +1,103 @@
+@extends('administration.students.student')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header text-center bg-info"><h2>Pré-inscription<h2></div>
+                <div class="card-body">
+                    <form method="post" action="{{ Route('students_store') }}">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
+                            <div class="col-md-6">
+                                <input class="form-control" type="text" placeholder="{{ Auth::user()->name }}" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="firstName" class="col-md-4 col-form-label text-md-right">{{ __('Prénom') }}</label>
+                            <div class="col-md-6">
+                                <input class="form-control" type="text" placeholder="{{ Auth::user()->firstname }}" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="scoolName" class="col-md-4 col-form-label text-md-right">{{ __('Ecole') }}</label>
+                            <div class="col-md-6">
+                                <input id="scoolName" type="text" class="form-control @error('scoolName') is-invalid @enderror" name="scoolName" value="{{ old('scoolName') }}" required autocomplete="scoolName" autofocus>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="year" class="col-md-4 col-form-label text-md-right">{{ __('Année scolaire') }}</label>
+                            <div class="col-md-6">
+                                <select class="form-control" id="year" type="text" name="year" value="{{ old('year') }}">
+                                    <option>Cp</option>
+                                    <option>Ce1</option>
+                                    <option>Ce2</option>
+                                    <option>Cm1</option>
+                                    <option>Cm2</option>
+                                    <option>6éme</option>
+                                    <option>5éme</option>
+                                    <option>4éme</option>
+                                    <option>3éme</option>
+                                    <option>2éme</option>
+                                    <option>1ére</option>
+                                    <option>Terminal</option>
+                                    <option>Université</option>
+                                    <option>Cours adultes</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="matter" class="col-md-4 col-form-label text-md-right">{{ __('Matière souhaité') }}</label>
+                            <div class="col-md-6">
+                                <select class="form-control" id="matter" type="text" name="matter" value="{{ old('matter') }}">
+                                <option>Mathematique</option>
+                                <option>Anglais</option>
+                                <option>Histoire</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="responsible" class="col-md-4 col-form-label text-md-right">{{ __('Responsable légal') }}</label>
+                            <div class="col-md-6">                   
+                                <input id="responsible" type="text" class="form-control @error('responsible') is-invalid @enderror" name="responsible" value="{{ old('responsible') }}" required autocomplete="responsible" autofocus>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Adresse') }}</label>
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+                            <div class="col-md-6">
+                                <input class="form-control" type="text" placeholder="{{ Auth::user()->email }}" readonly>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="phoneNumber" class="col-md-4 col-form-label text-md-right">{{ __('Téléphone') }}</label>
+                            <div class="col-md-6">
+                                <input id="phoneNumber" type="text" class="form-control @error('phoneNumber') is-invalid @enderror" name="phoneNumber" value="{{ old('phoneNumber') }}" required autocomplete="phoneNumber" autofocus>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Envoyer') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>    
+</div>
+@endsection
---
+++ resources/views/administration/students/student.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="jumbotron bg-dark text-white">
+        <h1>Bienvenue {{ Auth::user()->firstname }}</h1>      
+    </div>
+    <h4>Tu est dans t'on espace client.<br>Ici tu peut :</h4>
+    <div class="row">
+        <div class="col-lg-4 mb-3">
+            <div class="list-group" id="list-tab" role="tablist">
+                <a class="list-group-item list-group-item-action list-group-item-info" id="list-home-list" href="{{route('student_create')}}">Remplir t'a pré-inscription</a>
+                <a class="list-group-item list-group-item-action list-group-item-info" id="list-profile-list" href="{{route('student_edit')}}">Renouveler t'a pré-inscription</a>
+                <a class="list-group-item list-group-item-action list-group-item-info" id="list-messages-list" href="{{route('student_comment')}}">Laisser un commentaire</a>
+            </div>
+            @if(session()->has('info'))
+                <div class="alert alert-success mt-2">
+                    {{ session('info') }}
+                </div>
+            @endif
+        </div>
+        <div class="col-lg-8">
+            <div class="tab-content" id="nav-tabContent">
+                
+                @yield('container')
+
+            </div>
+        </div>
+    </div>   
+</div>
+@endsection
+
