cde01545a117bf04713eca109be2227d0a077f1a
helderleal <helder.leal.francisco@gmail.com>
2020-09-25 15:51:27 +0200

    dynamic display of matters in select

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Matter;
 use App\Student;
 use Illuminate\Http\Request;
 
@@ -42,7 +43,8 @@ class StudentsController extends Controller
      */
     public function create()
     {
-        return view('administration.students.actions.newStudent');
+        $matters = Matter::all();
+        return view('administration.students.actions.newStudent', compact('matters'));
     }
 
     /**
