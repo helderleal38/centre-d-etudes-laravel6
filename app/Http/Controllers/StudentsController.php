8a07f782134c45c9236de4bbdbe17281fedf8648
helderleal <helder.leal.francisco@gmail.com>
2020-10-05 18:00:14 +0200

    student request

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Matter;
 use App\Student;
 use Illuminate\Http\Request;
+use App\Http\Requests\StudentRequest;
 
 class StudentsController extends Controller
 {
@@ -53,7 +54,7 @@ class StudentsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StudentRequest $request)
     {
         Student::create([
             'scoolName' => Request('scoolName'), 
