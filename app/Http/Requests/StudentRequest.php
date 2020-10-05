af38a92acdc1e3acbc989f78fde34290f624f62a
helderleal <helder.leal.francisco@gmail.com>
2020-10-05 23:31:11 +0200

    setting up regex for validation

--- app/Http/Requests/StudentRequest.php
+++ app/Http/Requests/StudentRequest.php
@@ -24,12 +24,12 @@ class StudentRequest extends FormRequest
     public function rules()
     {
         return [
-            "scoolName" => "required|alpha_dash|min:5,max:30",
+            "scoolName" => "required|regex:/^[0-9\pL\s\-\']{5,50}+$/u",
             "year" => "required",
             "matter" => "required",
-            "responsible" => "required|alpha|min:3,max:30",
-            "address" => "required|alpha_dash|min:5,max:80",
-            "phoneNumber" => "required|digits:10",
+            "responsible" => "required|regex:/^[\pL\s]{3,25}+$/u",
+            "address" => "required|regex:/^[0-9\pL\s\-\'\,]{5,50}+$/u",
+            "phoneNumber" => "required|regex:/^[0-9\s\-\+\(\)]{9,20}$/",
         ];
     }
 
@@ -37,26 +37,20 @@ class StudentRequest extends FormRequest
     {
         return [
             "scoolName.required" => "<span style='color:red;'>Ce champ est obligatoire.</span>",
-            "scoolName.alpha_dash" => "<span style='color:red;'>Ce champ doit contenir uniquement des lettres, des chiffres et des tirets.</span>",
-            "scoolName.min" => "<span style='color:red;'>Ce champ doit contenir au moins 5 caractères.</span>",
-            "scoolName.max" => "<span style='color:red;'>Ce champ ne peut contenir plus de 30 caractères.</span>",
+            "scoolName.regex" => "<span style='color:red;'>Veuillez entrer des caractères valides.</span>",
 
             "year.required" => "<span style='color:red;'>Ce champ est obligatoire.</span>",
 
             "matter.required" => "<span style='color:red;'>Ce champ est obligatoire.</span>",
 
             "responsible.required" => "<span style='color:red;'>Ce champ est obligatoire.</span>",
-            "responsible.alpha" => "<span style='color:red;'>Ce champ doit contenir uniquement des lettres.</span>",
-            "responsible.min" => "<span style='color:red;'>Ce champ doit contenir au moins 3 caractères.</span>",
-            "responsible.max" => "<span style='color:red;'>Ce champ ne peut contenir plus de 30 caractères.</span>",
+            "responsible.regex" => "<span style='color:red;'>Veuillez entrer des caractères valides.</span>",
 
             "address.required" => "<span style='color:red;'>Ce champ est obligatoire.</span>",
-            "address.alpha_dash" => "<span style='color:red;'>Ce champ doit contenir uniquement des lettres, des chiffres et des tirets.</span>",
-            "address.min" => "<span style='color:red;'>Ce champ doit contenir au moins 5 caractères.</span>",
-            "address.max" => "<span style='color:red;'>Ce champ ne peut contenir plus de 80 caractères.</span>",
+            "address.regex" => "<span style='color:red;'>Veuillez entrer des caractères valides.</span>",
 
             "phoneNumber.required" => "<span style='color:red;'>Ce champ est obligatoire.</span>",
-            "phoneNumber.digits" => "<span style='color:red;'>Ce champ doit contenir 10 chiffres.</span>",
+            "phoneNumber.regex" => "<span style='color:red;'>Veuillez entrer un numero de telephone valide</span>",
         ];
     }
 }
