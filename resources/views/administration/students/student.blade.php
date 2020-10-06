3d94567915f6d8d2fc278cc80e82b0da8fb8cf27
helderleal <helder.leal.francisco@gmail.com>
2020-10-06 23:02:56 +0200

    delete conditions

--- resources/views/administration/students/student.blade.php
+++ resources/views/administration/students/student.blade.php
@@ -3,41 +3,31 @@
 @section('content')
 
 <div class="container">
-    <div class="jumbotron eleve bg-dark text-white">
-        <h1>Bienvenue {{ Auth::user()->firstname }}</h1>      
+  <div class="jumbotron eleve bg-dark text-white">
+      <h1>Bienvenue {{ Auth::user()->firstname }}</h1>      
+  </div>
+  <h4>Tu est dans t'on espace client.<br>Ici tu peut :</h4>
+  <div class="row">
+    <div class="col-lg-4 mb-3">
+      <div class="list-group" id="list-tab" role="tablist">
+        <a class="list-group-item list-group-item-action list-group-item-info" id="list-home-list" href="{{route('student_create')}}">Remplir t'a pré-inscription</a>
+        <a class="list-group-item list-group-item-action list-group-item-info" id="list-profile-list" href="{{route('student_edit')}}">Renouveler t'a pré-inscription</a>
+        <a class="list-group-item list-group-item-action list-group-item-info" id="list-messages-list" href="{{route('student_comment')}}">Laisser un commentaire</a>
+      </div>
+      @if(session()->has('success'))
+        <div class="alert alert-success mt-2">
+            {{ session('success') }}
+        </div>
+      @endif
     </div>
-    <h4>Tu est dans t'on espace client.<br>Ici tu peut :</h4>
-    <div class="row">
-        <div class="col-lg-4 mb-3">
-            <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action list-group-item-info" id="list-home-list" href="{{route('student_create')}}">Remplir t'a pré-inscription</a>
-                <a class="list-group-item list-group-item-action list-group-item-info" id="list-profile-list" href="{{route('student_edit')}}">Renouveler t'a pré-inscription</a>
-                <a class="list-group-item list-group-item-action list-group-item-info" id="list-messages-list" href="{{route('student_comment')}}">Laisser un commentaire</a>
-            </div>
-            @if(session()->has('info'))
-                <div class="alert alert-success mt-2">
-                    {{ session('info') }}
-                </div>
-            @endif
-            @if(session()->has('success'))
-                <div class="alert alert-success mt-2">
-                    {{ session('success') }}
-                </div>
-            @endif
-            @if(session()->has('errors'))
-                <div class="alert alert-danger mt-2">
-                    {{ session('errors') }}
-                </div>
-            @endif
-        </div>
-        <div class="col-lg-8">
-            <div class="tab-content" id="nav-tabContent">
-                
-                @yield('container')
+    <div class="col-lg-8">
+      <div class="tab-content" id="nav-tabContent">
 
-            </div>
-        </div>
-    </div>   
+          @yield('container')
+          
+      </div>
+    </div>
+  </div>   
 </div>
 @endsection
 
