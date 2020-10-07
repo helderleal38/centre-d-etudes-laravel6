025fe2ccb34c298d00ed54d89483386347649cb0
helderleal <helder.leal.francisco@gmail.com>
2020-10-07 18:26:47 +0200

    modal inside foreach

--- resources/views/administration/administrateur/mails/index.blade.php
+++ resources/views/administration/administrateur/mails/index.blade.php
@@ -42,41 +42,44 @@
                       </div>
                     </td>
                   </tr>
+
+                  <!---------------------------------------------------------------------------------------------------------------->
+                  <!------------------------------------------------- MAIL MODAL ------------------------------------------------------>
+                  <!---------------------------------------------------------------------------------------------------------------->
+                  <div class="modal fade" id="mailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                      <div class="modal-content ">
+                        <div class="modal-header">
+                          <h2>{{$contact->subject}}</h2>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="card-body">
+                          <p>DE : {{$contact->name}}</p>
+                        </div>
+                        <div class="modal-body">
+                          <div class="card-body" text='centered' style="background-color: #f4f6f9">{{$contact->message}}</div>
+                        </div>
+                        <div class="card-body">
+                          <p>{{$contact->name}}</p>
+                          <p>{{$contact->email}}</p>
+                          <p>{{$contact->phone}}</p>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 @endforeach
               </tbody>            
             </table>
           </div>
         </div>
       </div>
-      <!---------------------------------------------------------------------------------------------------------------->
-  <!------------------------------------------------- MAIL MODAL ------------------------------------------------------>
-  <!---------------------------------------------------------------------------------------------------------------->
-  <div class="modal fade" id="mailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content ">
-        <div class="modal-header">
-          <h2>{{$contact->subject}}</h2>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="card-body">
-          <p>DE : {{$contact->name}}</p>
-        </div>
-        <div class="modal-body">
-          <div class="card-body" text='centered' style="background-color: #f4f6f9">{{$contact->message}}</div>
-        </div>
-        <div class="card-body">
-          <p>{{$contact->name}}</p>
-          <p>{{$contact->email}}</p>
-          <p>{{$contact->phone}}</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-        </div>
-      </div>
-    </div>
-  </div>
+ 
+ 
 
     </div>  
   </div>
