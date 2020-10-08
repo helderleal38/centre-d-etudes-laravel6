f2385901912759c018ef77d5db2e58b34afaf582
helderleal <helder.leal.francisco@gmail.com>
2020-10-09 01:15:31 +0200

    add content

--- resources/views/frontend/accueil.blade.php
+++ resources/views/frontend/accueil.blade.php
@@ -79,7 +79,7 @@
         <div class="col-lg-3 col-md-6 mb-md-0 mb-5">
           <i class="fas fa-chart-line fa-3x green-text mb-4"></i>
           {{-- <i class="fas fa-chart-line"></i> --}}
-          <h4 class="font-weight-bold mb-4">Reussite garantie</h4>
+          <h4 class="font-weight-bold mb-4">REUSSITE GARANTIE</h4>
           <p class="text-muted px-2 mb-md-0">
             Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. 
           </p>
