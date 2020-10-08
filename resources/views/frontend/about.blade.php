80a2422637b7ed1b459e4012e52db367f1ebd640
helderleal <helder.leal.francisco@gmail.com>
2020-10-09 01:15:09 +0200

    change text

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -99,9 +99,9 @@
               class="img-fluid rounded-circle z-depth-1"/>
           </div>
           <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Alan Turing</h6>
+            <h6 class="font-weight-bold pt-2">Vasco Soares</h6>
             <p class="text-muted">
-              <small><i>Lorem ipsum dolor sit amet, consectetur adipiscingelit.</i></small>
+              <small><i>Administrateur de l'ecole et professeur de mathématique.</i></small>
             </p>
           </div>
         </div>
@@ -116,7 +116,7 @@
           <div class="text-center mt-3">
             <h6 class="font-weight-bold pt-2">Tom Johnson</h6>
             <p class="text-muted">
-              <small><i>Lorem ipsum dolor sit amet, consectetur adipiscingelit.</i></small>
+              <small><i>Professeur d'anglais.</i></small>
             </p>
           </div>
         </div>
@@ -130,7 +130,7 @@
           <div class="text-center mt-3">
             <h6 class="font-weight-bold pt-2">Alex Laurie</h6>
             <p class="text-muted">
-              <small><i>Lorem ipsum dolor sit amet, consectetur adipiscingelit.</i></small>
+              <small><i>Professeur de technologies.</i></small>
             </p>
           </div>
         </div>
@@ -144,49 +144,7 @@
           <div class="text-center mt-3">
             <h6 class="font-weight-bold pt-2">Nathalie Fry</h6>
             <p class="text-muted">
-              <small><i>Lorem ipsum dolor sit amet, consectetur adipiscingelit.</i></small>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-        <div class="p-4">
-          <div class="avatar w-100 white d-flex justify-content-center align-items-center">
-            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
-              class="img-fluid rounded-circle z-depth-1"/>
-          </div>
-          <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Emma Lovelace</h6>
-            <p class="text-muted">
-              <small><i>Lorem ipsum dolor sit amet, consectetur adipiscingelit.</i></small>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-        <div class="p-4">
-          <div class="avatar w-100 white d-flex justify-content-center align-items-center">
-            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(5).jpg"
-              class="img-fluid rounded-circle z-depth-1"/>
-          </div>
-          <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Joanna Thompson</h6>
-            <p class="text-muted">
-              <small><i>Lorem ipsum dolor sit amet, consectetur adipiscingelit.</i></small>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-        <div class="p-4">
-          <div class="avatar w-100 white d-flex justify-content-center align-items-center">
-            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg"
-              class="img-fluid rounded-circle z-depth-1"/>
-          </div>
-          <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Billy Turner</h6>
-            <p class="text-muted">
-              <small><i>Lorem ipsum dolor sit amet, consectetur adipiscingelit.</i></small>
+              <small><i>Professeur de histoire.</i></small>
             </p>
           </div>
         </div>
