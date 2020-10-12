4a7bad76aa668014d77d1e025ec64cc5fe403ccf
helderleal <helder.leal.francisco@gmail.com>
2020-10-13 00:01:05 +0200

    change id

--- resources/views/administration/students/actions/commentStudent.blade.php
+++ resources/views/administration/students/actions/commentStudent.blade.php
@@ -34,9 +34,9 @@
               </div>
 
               <div class="form-group row">
-                <label for="exampleFormControlFile1" class="col-md-4 col-form-label text-md-right">Votre photo/avatar</label>
+                <label for="avatar" class="col-md-4 col-form-label text-md-right">Votre photo/avatar</label>
                 <div class="col-md-6">
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1" name="avatar">
+                    <input type="file" class="form-control-file" id="avatar" name="avatar">
                     {!! $errors->first('avatar', ":message") !!}
                 </div>
               </div>
